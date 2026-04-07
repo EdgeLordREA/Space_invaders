af3141605edf31f13f8d4326d0020d82e2ba6482
Noah REA <nbuch@reacollege.eu>
2026-04-07 12:58:39 +0200

    refactor: Integrate new modules and wire input actions in main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use macroquad::input::is_key_down;
-use macroquad::miniquad::KeyCode;
+pub mod actions;
+pub mod player;
+pub mod bullet;
+pub mod gamestate;
+
+use macroquad::*;
+use macroquad::input::{is_key_down, KeyCode};
 
 fn main() {
     println!("Hello, world!");
-    let r = is_key_down(KeyCode::A);
 }
 
 fn handle_inputs()
 {
-    if is_key_down(KeyCode::A) {  }
+    if is_key_down(KeyCode::W) {actions::do_w()}
+    if is_key_down(KeyCode::A) {actions::do_a()}
+    if is_key_down(KeyCode::S) {actions::do_s()}
+    if is_key_down(KeyCode::D) {actions::do_d()}
 }
+
