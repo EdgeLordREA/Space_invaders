11e5dff3233aa3306fecb188637cc0c5fd10b579
Noah REA <nbuch@reacollege.eu>
2026-04-08 10:39:33 +0200

    feat: Introduce GameState struct and constructor

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -1,8 +1,20 @@
 use crate::objects::*;
-
 pub struct GameState
 {
     player : player::Player,
-    vec: Vec<bullet::Bullet>,
+    bullets : Vec<bullet::Bullet>,
+    enemies : Vec<enemy::Enemy>
 
+}
+
+impl GameState {
+    pub(crate) fn new(player : player::Player) -> GameState {
+        let bullets : Vec<bullet::Bullet> = Vec::new();
+        let enemies : Vec<enemy::Enemy> = Vec::new();
+        GameState{
+            player,
+            bullets,
+            enemies
+        }
+    }
 }
