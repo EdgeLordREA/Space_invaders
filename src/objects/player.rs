cb35721521e35451854d3f22c10b027139e25a37
Noah REA <nbuch@reacollege.eu>
2026-04-08 10:39:32 +0200

    feat: Introduce Player object and constructor

--- src/objects/player.rs
+++ src/objects/player.rs
@@ -1,4 +1,5 @@
 use macroquad::math::Vec2;
+use macroquad::window::{screen_height, screen_width};
 
 pub struct Player
 {
@@ -18,5 +19,15 @@ impl Player
     pub fn attack(&mut self)
     {
         self.atk_cooldown = 1.0 / self.atk_speed
+    }
+
+    pub const fn new(screen_width : f32, screen_height : f32) -> Player {
+        Player {
+            position: Vec2::new(screen_width/2.0, screen_height-10.0), // Start at the origin
+            speed: 200.0,                  // Base movement speed
+            atk_speed: 1.0,                // 1 attack per second
+            atk_cooldown: 0.0,             // Ready to attack immediately
+            dmg: 10.0,                     // Base damage
+        }
     }
 }
